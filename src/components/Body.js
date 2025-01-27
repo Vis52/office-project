01fbd7da81c4e3f96a69cbaf6c004b7ea679d6db
vis52 <vs6773817@gmail.com>
2025-01-27 18:12:49 +0530

    initial commit

---
+++ src/components/Body.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Body.css';
+import images   from'../utils/img/images.jpg';
+
+function Body() {
+    return (
+    
+        <div >
+            <p class='line1'><b>SKIN CLEANSER</b></p><br></br>
+            <b> <b><h1>Balancing Gel<br></br>Cleanser</h1><br></br></b>
+                <p class='line2'>A balancing gel Cleanser is a gentle yet effective skincare product <br></br>
+                    design to remove impurities, excess oil,and make up while<br></br>
+                    maintaining the skin's natural ph balance.</p></b>
+            <div>
+                <button class='v1'>Explore Now</button>
+                 <button classs='v2'> How To Buy</button>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default Body;
---
+++ src/components/Header.js
@@ -0,0 +1,48 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../utils/img/logo.png';
+import Search from '../utils/img/search.png';
+import user from '../utils/img/user.jpg';
+import Button from 'react-bootstrap/esm/Button';
+import './Navbar.css'
+
+function Header() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container> <Navbar.Brand href="#home">
+        <img src={logo} alt="Cleanser Logo" /> Cleanser
+      </Navbar.Brand> <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavDropdown className='my-auto' title="Product"
+              id="product-dropdown">
+              <NavDropdown.Item className='my-auto' href="#product/our-products">Our Products</NavDropdown.Item>
+              <NavDropdown.Item className='my-auto' href="#product/best-selling">Best Selling Product</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#story">Story</Nav.Link>
+            <Nav.Link href="#manufacturing">Manufacturing</Nav.Link>
+            <NavDropdown title="Packazine" id="packazine-dropdown">
+              <NavDropdown.Item href="#packazine/without-plastic">Without Plastic</NavDropdown.Item>
+              <NavDropdown.Item href="#packazine/environment-supporting">Environment Supporting</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href='Search here'>
+
+              <img src={Search} alt='Search here' /> 
+            </Nav.Link>
+            <Nav.Link href='user'>
+              <img src={user} alt='user' /> </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+
+
+
+      </Container>
+    </Navbar>
+
+  );
+}
+export default Header;
+
+
